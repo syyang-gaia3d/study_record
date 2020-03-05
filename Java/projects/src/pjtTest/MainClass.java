27bbacb28b5ce156e4845b5e1f072989c3c1307a
syyang <syyang@gaia3d.com>
2020-03-05 17:16:09 +0900

    Java 학습_자바 프로그래밍 입문 강좌 3강

--- Java/projects/src/pjtTest/MainClass.java
+++ Java/projects/src/pjtTest/MainClass.java
@@ -4,7 +4,26 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Java World!!");
+		//1강
+		//System.out.println("Hello Java World!!");
+		
+		//3강
+		int i = 10;
+		int j;
+		j = 100;
+		
+		i = 200;
+		
+		System.out.println("i + j = " + (i+j));
+		
+		int a = 0;
+		System.out.println("a = " + a);
+		a = 100;
+		System.out.println("a = " + a);
+		a = 300;
+		System.out.println("a = " + a);
+		a = 0;
+		System.out.println("a = " + a);
 		
 	}
 	
